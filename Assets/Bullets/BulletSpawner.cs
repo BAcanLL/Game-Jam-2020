d033c2811f147932f80638a31b96a6f502eed72b
Edwin Lin <elin4527@gmail.com>
2020-02-01 11:34:36 -0500

    Added bullets + spawner

---
+++ Assets/Bullets/BulletSpawner.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bullet;
+    public float bulletSpeed;
+    public float range;
+    public int bulletDamage;
+    public float cooldown;
+    
+    float cooldownExpireTime;
+    Vector3 rotation;
+    bool isFiring;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFiring = false;
+        cooldownExpireTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldown > 0)
+        {
+            cooldownExpireTime += Time.deltaTime;
+        }
+        if (isFiring && cooldownExpireTime >= cooldown)
+        {
+            cooldownExpireTime -= cooldown;
+
+            Rotate(new Vector3(0, 0, 30));
+
+            FireBullet();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void SetFiring(bool fire)
+    {
+        isFiring = fire;
+    }
+
+    public void SetRotation(Vector3 rotation)
+    {
+        this.rotation = rotation;
+    }
+
+    public void Rotate(Vector3 rotation)
+    {
+        this.rotation += rotation;
+    }
+
+    private void FireBullet()
+    {
+        GameObject g = Instantiate(bullet);
+        Bullet b = g.GetComponent<Bullet>();
+        b.transform.position = transform.position;
+        b.transform.rotation = transform.rotation;
+        b.speed = bulletSpeed;
+        b.damage = bulletDamage;
+        b.range = range;
+        b.setDirection(rotation);
+    }
+}
---
+++ Assets/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+    public static Vector3 convertRotationToDirection(Vector3 rotation)
+    {
+        return Quaternion.Euler(rotation) * Vector3.up;
+    }
+
+    public static Vector3 convertRotationToDirection(Quaternion rotation)
+    {
+        return rotation * Vector3.up;
+    }
+}
