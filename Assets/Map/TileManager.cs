2f41e9ba9a4b24d73fb99bcc011fb4d7fb88ba4e
23scurtu <23scurtuvictor@gmail.com>
2020-02-01 14:23:08 -0500

    Add tilemap skeleton

---
+++ Assets/Map/TileManager.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+enum TileType
+{
+    GRASS, BRICK
+}
+
+public class TileManager : MonoBehaviour
+{
+    List<Block> tiles;
+    public Tilemap initialization_map;
+
+    Tilemap NE_map;
+    Tilemap SE_map;
+    Tilemap SW_map;
+    Tilemap NW_map;
+
+    Block createTile(TileType type)
+    {
+        return null;
+        // switch{
+            
+
+        // }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
