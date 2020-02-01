f5a012a3839f26e12935dc6ecce07ef3f64b1d99
Alex Bie <alexbie98@gmail.com>
2020-02-01 17:09:17 -0500

    view setup

---
+++ Assets/ViewManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewManager : MonoBehaviour
+{
+    public GameObject initialization_view;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // create empty tilemaps
+        // for tile in initialization_view
+            // create logical blocks
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
