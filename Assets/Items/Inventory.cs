01b537d2291a6143ce54737949062fea1c567465
Edwin Lin <elin4527@gmail.com>
2020-02-01 14:16:55 -0500

    Created inventory

--- Assets/Items/Inventory.cs
+++ Assets/Items/Inventory.cs
@@ -2,42 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public class Inventory
 {
-    private struct Collection
+    private class Collection
     {
         public int count;
         public Item item;
     }
 
-    public GameObject[] items;
     private List<Collection> collections;
-    private int activeIndex;
-
-    Inventory()
+    public Inventory()
     {
         collections = new List<Collection>();
     }
 
-    void OnStart()
-    {
-        foreach (GameObject g in items)
-        {
-            Item item = g.GetComponent<Item>();
-            if (item)
-            {
-                createCollection(item);
-            }
-        }
-    }
-
-
-    private void createCollection(Item item)
+    public void CreateCollection(Item item)
     {
         Collection c = new Collection();
         c.count = 0;
         c.item = item;
 
         collections.Add(c);
+    }
+
+    public void UseItem(int index)
+    {
+        if (index >= 0 && index < collections.Count && collections[index].count > 0)
+        {
+            Collection c = collections[index];
+            c.count--;
+            collections[index] = c;
+        }
+    }
+
+    public Item GetItem(int index)
+    {
+        if (index >= 0 && index < collections.Count)
+        {
+            return collections[index].item;
+        }
+        return null;
+    }
+
+    public int GetItemCount(int index)
+    {
+        if (index >= 0 && index < collections.Count)
+        {
+            return collections[index].count;
+        }
+        return 0;
+    }
+
+    public bool AddItem(Item item)
+    {
+        foreach (Collection c in collections)
+        {
+            if (c.item.itemID == item.itemID)
+            {
+                c.count += 1;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int GetSize()
+    {
+        return collections.Count;
     }
 }
